2714d1d796594cfda50922030259c42ac48b78e3
dim0nickb <basstorniloo@gmail.com>
2018-09-24 14:32:40 +0300

    test job

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  /**Active Tab */
+  selectedIndex = 0;
+
+  key = 'ui-task-selectedIndex';
+
+  OnSelectedIndexChange(idx: number): void {
+    localStorage.setItem(this.key, idx.toString());
+  }
+
+  ngOnInit(): void {
+    const selectedIndex = localStorage.getItem(this.key);
+    if (selectedIndex && selectedIndex.length) {
+      this.selectedIndex = +selectedIndex;
+    }
+  }
+
+
 }
